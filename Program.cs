6ebee5b66db2c830f49cce9e7d534e2cc65c68fc
José Guilherme Lins Filho <jose.filho@elecnor.com.br>
2023-08-11 13:44:37 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("\nBEM VINDO AO EDITOR TOPZEIRA\n");
+            Console.WriteLine("1 - Abrir arquivo\n2 - Criar arquivo\n3 - Sair\n------------------");
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1: Abrir();break;
+                case 2: Editar(); break;
+                case 3: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+        }
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo  (ESC para sair)");
+            Console.WriteLine("\n---------------------");
+
+            string texto = "";
+
+            do
+            {
+                texto += Console.ReadLine();
+                texto += Environment.NewLine;
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape)
+            
+            Console.WriteLine(texto)
+        }
+    }
+}
